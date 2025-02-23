ccd30a4e703d120d3cfd474fc87f2ab6004fa148
Taariq Mansurie <Taariqmasnurie@gmail.com>
2025-02-23 15:05:16 -0800

    added farhenheit to celcius swift file

--- FarhenheitToCelciusConvertor.playground/Contents.swift
+++ FarhenheitToCelciusConvertor.playground/Contents.swift
@@ -2,4 +2,4 @@ import Cocoa
 
 let celcius =  25.0
 let fahrenheit = celcius * 9/5 + 32
-print("\(celcius) is in celcius & it is equal to \(fahrenheit) in fahrenheit")
+print("\(celcius) is in celcius & it is equal to \(fahrenheit) in fahrenheit")   
